15dced0d3aa9707df9465ac0bef86070e5c8b011
philippe-laurentiu <laurentiu.philippe@gmail.com>
2022-10-23 11:03:07 -0700

    initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Landingpage from './pages/Landingpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Landingpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/pages/Landingpage.tsx
@@ -0,0 +1,8 @@
+import React, {} from 'react'
+
+
+const Landingpage = () => {
+    return <p>hello</p>
+}
+
+export default Landingpage
