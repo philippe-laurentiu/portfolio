056fff8e4cecd35b6fb1263cb6c565d40ae34597
philippe-laurentiu <laurentiu.philippe@gmail.com>
2022-12-15 18:44:03 -0800

    add contentnt to project test site

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -12,32 +12,32 @@ const Home = (): JSX.Element => {
   return (
     <Box>
       <Container>
-      <ImageWithText
-          projectImage={testImg}
-          background={gradient('rgba(0,0,0,0)', 'rgba(0,0,0,0)')}
-          titleLogoText={{
-            title: 'Hi there, my name is Philippe!',
-            text: "I'm a software developer based in Kelowna, and  I create high-performance software with the newest technologies."
-          }}
-        />
-        <Title>Projects</Title> {/* todo translation */}
         <ImageWithText
-          projectImage={testImg}
-          background={gradient('rgba(0,111,255,0)', 'rgba(9,9,121,0.4)')}
-          titleLogoText={{
-            title: t('imageWithText.project1.title'),
-            logo: testLogo,
-            text: t('imageWithText.project1.text')
-          }}
-        />
-        <ImageWithText
-          projectImage={testImg}
-          background={gradient('rgba(0,111,255,0.3)', 'rgb(121,71,9,0.8)')}
-          titleLogoText={{
-            title: t('imageWithText.project2.title'),
-            logo: testLogo,
-            text: t('imageWithText.project2.text')
-          }}
+            projectImage={testImg}
+            background={gradient('rgba(0,0,0,0)', 'rgba(0,0,0,0)')}
+            titleLogoText={{
+              title: 'Hi there, my name is Philippe!',
+              text: "I'm a software developer based in Kelowna, and  I create high-performance software with the newest technologies."
+            }}
+          />
+          <Title>Projects</Title> {/* todo translation */}
+          <ImageWithText
+            projectImage={testImg}
+            background={gradient('rgba(0,111,255,0)', 'rgba(9,9,121,0.4)')}
+            titleLogoText={{
+              title: t('imageWithText.project1.title'),
+              logo: testLogo,
+              text: t('imageWithText.project1.text')
+            }}
+          />
+          <ImageWithText
+            projectImage={testImg}
+            background={gradient('rgba(0,111,255,0.3)', 'rgb(121,71,9,0.8)')}
+            titleLogoText={{
+              title: t('imageWithText.project2.title'),
+              logo: testLogo,
+              text: t('imageWithText.project2.text')
+            }}
         />
       </Container>
     </Box>
--- app/src/pages/projects/ProExample.tsx
+++ app/src/pages/projects/ProExample.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { ImageWithText, Title } from '../PageFragments'
+import testImg from '../../assets/test.jpg'
+import { gradient } from '../../utils/calculateGradient'
+import Container from '@material-ui/core/Container' // todo @material-ui is not @mui/material
+import Box from '@mui/material/Box'
 
 const ProExampel = (): JSX.Element => {
-  return (<div>todo</div>)
+  return (
+    <Box>
+      <Container>
+        <ImageWithText
+            projectImage={testImg}
+            background={gradient('rgba(0,0,0,0)', 'rgba(0,0,0,0)')}
+            titleLogoText={{
+              title: 'Hi there, my name is Philippe!',
+              text: "I'm a software developer based in Kelowna, and  I create high-performance software with the newest technologies."
+            }}
+          />
+          <Title>Projects</Title> {/* todo translation */}
+      </Container>
+    </Box>
+  )
 }
 
 export default ProExampel
